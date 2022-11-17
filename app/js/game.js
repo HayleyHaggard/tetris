47481c354268df7a7045a3948e8d190b5cd3dd45
HayleyHaggard <nastena_panova2000@mail.ru>
2022-11-17 17:17:17 +0300

    added screens, figures, panel of score

--- app/js/game.js
+++ app/js/game.js
@@ -1,68 +1,130 @@
 export default class Game {
+  static points = {
+    '1': 40,
+    '2': 100,
+    '3': 300,
+    '4': 1200
+  };
+
   score = 0;
   lines = 0;
-  level = 0;
-  playfield = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0,],
-  ];
+  playfield = this.createPlayfield();
+  activePiece = this.createPiece();
+  nextPiece = this.createPiece();
 
-  activePiece = {
-    x: 0,
-    y: 0,
-    blocks: [
-      [0, 1, 0],
-      [1, 1, 1],
-      [0, 0, 0]
-    ],
+  get level() {
+    return Math.floor(this.lines * 0.1);
+  }
 
-    //another realisation
+  getState() {
+    const playfield = this.createPlayfield();
+    const { y: pieceY, x: pieceX, blocks } = this.activePiece;
 
-    // get blocks() {
-    //   return this.rotations[this.rotationIndex];
-    // },
-    // rotationIndex: 0,
-    // rotations: [
-    //   [
-    //     [0, 1, 0],
-    //     [1, 1, 1],
-    //     [0, 0, 0]
-    //   ],
-    //   [
-    //     [0, 1, 0],
-    //     [0, 1, 1],
-    //     [0, 1, 0]
-    //   ],
-    //   [
-    //     [0, 0, 0],
-    //     [1, 1, 1],
-    //     [0, 1, 0]
-    //   ],
-    //   [
-    //     [0, 1, 0],
-    //     [1, 1, 0],
-    //     [0, 1, 0]
-    //   ]
-    // ]
-  };
+    for (let y = 0; y < this.playfield.length; y++) {
+      playfield[y] = [];
+
+      for (let x = 0; x < this.playfield[y].length; x++) {
+        playfield[y][x] = this.playfield[y][x];
+      }
+    }
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x]) {
+          playfield[pieceY + y][pieceX + x] = blocks[y][x];
+        }
+      }
+    }
+
+    return {
+      score: this.score,
+      level: this.level,
+      lines: this.lines,
+      nextPiece: this.nextPiece,
+      playfield
+    };
+  }
+
+  createPlayfield() {
+    const playfield = [];
+
+    for (let y = 0; y < 20; y++) {
+      playfield[y] = [];
+
+      for (let x = 0; x < 10; x++) {
+        playfield[y][x] = 0;
+      }
+    }
+
+    return playfield;
+  }
+
+  createPiece() {
+    const index = Math.floor(Math.random() * 7);
+    const type = 'IJLOSTZ'[index];
+    const piece = {};
+
+    switch (type) {
+      case 'I':
+        piece.blocks = [
+          [0, 0, 0, 0],
+          [1, 1, 1, 1],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0]
+        ];
+        break;
+      case 'J':
+        piece.blocks = [
+          [0, 0, 0],
+          [2, 2, 2],
+          [0, 0, 2]
+        ];
+        break;
+      case 'L':
+        piece.blocks = [
+          [0, 0, 0],
+          [3, 3, 3],
+          [3, 0, 0]
+        ];
+        break;
+      case 'O':
+        piece.blocks = [
+          [0, 0, 0, 0],
+          [0, 4, 4, 0],
+          [0, 4, 4, 0],
+          [0, 0, 0, 0]
+        ];
+        break;
+      case 'S':
+        piece.blocks = [
+          [0, 0, 0],
+          [0, 5, 5],
+          [5, 5, 0]
+        ];
+        break;
+      case 'T':
+        piece.blocks = [
+          [0, 0, 0],
+          [6, 6, 6],
+          [0, 6, 0]
+        ];
+        break;
+      case 'Z':
+        piece.blocks = [
+          [0, 0, 0],
+          [7, 7, 0],
+          [0, 7, 7]
+        ];
+        break;
+      default:
+        throw new Error('Неизестный тип фигуры');
+    }
+
+    piece.x = Math.floor((10 - piece.blocks[0].length) / 2)
+    piece.y = -1;
+
+    return piece;
+  }
 
   movePieceLeft() {
     this.activePiece.x -= 1;
@@ -86,6 +148,44 @@ export default class Game {
     if (this.hasCollision()) {
       this.activePiece.y -= 1;
       this.lockPiece();
+      const clearedLines = this.clearLines();
+      this.updateScore(clearedLines);
+      this.updatePieces();
+    }
+  }
+
+  //rotate our piece in both directions
+  rotateBlocks(clockwise = true) {
+    const blocks = this.activePiece.blocks;
+    const length = blocks.length;
+    const x = Math.floor(length / 2);
+    const y = Math.floor(length - 1);
+
+    for (let i = 0; i < x; i++) {
+      for (let j = i; j < y - i; j++) {
+        const temp = blocks[i][j];
+
+        if (clockwise) {
+          blocks[i][j] = blocks[y - j][i];
+          blocks[y - j][i] = blocks[y - i][y - j];
+          blocks[y - i][y - j] = blocks[j][y - i];
+          blocks[j][y - i] = temp;
+        } else {
+          blocks[i][j] = blocks[j][y - i];
+          blocks[j][y - i] = blocks[y - i][y - j];
+          blocks[y - i][y - j] = blocks[y - j][i];
+          blocks[y - j][i] = temp;
+        }
+      }
+    }
+  }
+
+  //if our piece near the bound - we can't rotate it
+  rotatePiece() {
+    this.rotateBlocks();
+
+    if (this.hasCollision()) {
+      this.rotateBlocks(false);
     }
   }
 
@@ -102,46 +202,35 @@ export default class Game {
     }
   }
 
-  //if our piece near the bound - we can't rotate it
-  rotatePiece() {
-    this.rotateBlocks();
+  clearLines() {
+    const rows = 20;
+    const columns = 10;
+    let lines = [];
 
-    if (this.hasCollision()) {
-      this.rotateBlocks(false);
-    }
+    for (let y = rows - 1; y >= 0; y--) {
+      let numberOfBlocks = 0;
 
-    //another realisation without rotateBlocks()
-    // this.activePiece.rotationIndex = this.activePiece.rotationIndex < 3 ? this.activePiece.rotationIndex + 1 : 0;
-    // if (this.hasCollision()) {
-    //   this.activePiece.rotationIndex = this.activePiece.rotationIndex > 0 ? this.activePiece.rotationIndex - 1 : 3;
-    // }
-    // return this.activePiece.blocks;
-  }
-
-  //rotate our piece in both directions
-  rotateBlocks(clockwise = true) {
-    const blocks = this.activePiece.blocks;
-    const length = blocks.length;
-    const x = Math.floor(length / 2);
-    const y = Math.floor(length - 1);
-
-    for (let i = 0; i < x; i++) {
-      for (let j = i; j < y - i; j++) {
-        const temp = blocks[i][j];
-
-        if (clockwise) {
-          blocks[i][j] = blocks[y - j][i];
-          blocks[y - j][i] = blocks[y - i][j];
-          blocks[y - i][y - j] = blocks[j][y - i];
-          blocks[j][y - i] = temp;
-        } else {
-          blocks[i][j] = blocks[j][y - i];
-          blocks[j][y - i] = blocks[y - i][y - j];
-          blocks[y - i][y - j] = blocks[y - j][i];
-          blocks[y - j][i] = temp;
+      for (let x = 0; x < columns; x++) {
+        if (this.playfield[y][x]) {
+          numberOfBlocks += 1;
         }
       }
+
+      if (numberOfBlocks === 0) {
+        break;
+      } else if (numberOfBlocks < columns) {
+        continue;
+      } else if (numberOfBlocks === columns) {
+        lines.unshift(y);
+      }
     }
+
+    for (let index of lines) {
+      this.playfield.splice(index, 1);
+      this.playfield.unshift(new Array(columns).fill(0));
+    }
+
+    return lines.length;
   }
 
   //check if our piece isn't out of bounds and if it didn't collide with another piece
@@ -161,5 +250,17 @@ export default class Game {
     }
 
     return false;
+  }
+
+  updateScore(clearedLines) {
+    if (clearedLines > 0) {
+      this.score += Game.points[clearedLines] * (this.level + 1);
+      this.lines += clearedLines;
+    }
+  }
+
+  updatePieces() {
+    this.activePiece = this.nextPiece;
+    this.nextPiece = this.createPiece();
   }
 }
